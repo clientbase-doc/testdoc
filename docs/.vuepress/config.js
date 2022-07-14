0104a1e6aabb57670cbf0e40a1315a265f268531
Andrei V. Goryunov <gor.a@clientbase.ru>
2022-07-14 16:34:55 +0300

    clientbase test documentation site

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,46 @@
+const { defaultTheme } = require('vuepress')
+
 module.exports = {
-  lang: 'en-US',
+  lang: 'ru-RU',
   base: '/testdoc/',
-  title: 'Clientbase Test Documentation',
-  description: 'This is my first VuePress documentation site',
+  title: 'Clientbase Test Doc',
+  description: 'clientbase test documentation site',
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: 'Начало работы',
+        link: '/guide/',
+      },
+      {
+        text: 'Руководство',
+        link: '/manual/',
+      },
+      {
+        text: 'Сайт КБ',
+        link: 'https://clientbase.ru/',
+      },
+      {
+        text: 'Заявка в ТП',
+        link: 'https://clientbase.ru/support/form.html',
+      },
+    ],
+    sidebar: {
+      '/manual/': [
+        {
+          text: 'Пользователю',
+          collapsible: true,
+          children: ['/manual/user/desktop/'],
+        },
+        {
+          text: 'Другое',
+          collapsible: true,
+          children: [
+            '/manual/other/disable_copyright/',
+            '/manual/other/footer_tuning/',
+          ],
+        },
+      ],
+    },
+  }),
+
 }
---
+++ docs/guide/index.md
@@ -0,0 +1,9 @@
+# Начало работы
+
+Быстрый старт
+
+<div style="text-align:center">
+
+![](/images/cb_logo_big.png)
+
+</div>
